d87a8853e571b9ad708d5ec0583f178262208120
lsiwh37249 <lsiwh37249@gmail.com>
2024-07-29 17:36:28 +0900

    movie 추가

--- dags/movie.py
+++ dags/movie.py
@@ -8,6 +8,22 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
+from pprint import pprint
+
+from airflow.operators.python import (
+        PythonOperator, PythonVirtualenvOperator
+        )
+
+def print_context(ds=None, **kwargs):
+    """Print the Airflow context and ds variable from the context."""
+    print("::group::All kwargs")
+    pprint(kwargs)
+    print("::endgroup::")
+    print("::group::Context variable ds")
+    print(ds)
+    print("::endgroup::")
+    return "Whatever you return gets printed in the logs"
+
 with DAG(
         'movie_datapipline',
     # These args will get passed on to each operator
@@ -24,15 +40,33 @@ with DAG(
     catchup=True,
     tags=['simple','bash', 'etl','shop'],
 ) as dag:
+   
+    def get_data(ds, **kwargs):
+        print(ds)
+        print(kwargs)
+        print(f"ds_nodash => {kwargs['ds_nodash']}")
+        print(f"kwargs type => {type(kwargs)}")
+        print('*'* 20)
+        from mov.api.call import list2df, get_key, save2df
+        #from home.kim1.code.new.mov.src.mov.api import gen_url, req, get_key, req2list, list2df, savedf
+        key = get_key()
+        print(key)
+        YYYYMMDD = kwargs['ds_nodash']
+        df = save2df(YYYYMMDD)
+        print(df.head(5))
+
+    run_this = PythonOperator(
+        task_id="print_the_context",
+        python_callable=print_context,
+    )
 
     # t1, t2 and t3 are examples of tasks created by instantiating operators
-    task_get = BashOperator(
+    task_get = PythonVirtualenvOperator(
         task_id='get.data',
-        bash_command="""
-            echo "get_date"
-        """ ,
-        trigger_rule="all_success"
-        )
+        python_callable=get_data,
+        requirements=["git+https://github.com/lsiwh37249/mov.git@0.2/api"],
+        system_site_packages=False,
+    )
 
     task_save = BashOperator(
         task_id='save.data',
@@ -40,19 +74,10 @@ with DAG(
             echo "save_date"
         """ ,
         trigger_rule="all_success"
-        )
-
-
-    task_err = BashOperator(
-        task_id='err.report',
-        bash_command="""
-            echo "err report"
-        """,
-        trigger_rule="one_failed"
     )
 
-#    task_done = BashOperator(
-#        task_id='make.done',
+
+#    task_err = BashOperator(
 #        bash_command="""
 #            DONE_PATH=~/data/done/{{ds_nodash}}
 #            mkdir -p ${DONE_PATH}
@@ -65,5 +90,5 @@ with DAG(
     task_start = EmptyOperator(task_id='start')
 
     task_start >> task_get >> task_save >> task_end 
-
+    task_start >> run_this >> task_end
 
